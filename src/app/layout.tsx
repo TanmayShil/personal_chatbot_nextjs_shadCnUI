893ef798d3d11e5570138ec0c777a6f4ee23bf78
TanmayShil <tanmayshil2002@gmail.com>
2026-03-04 14:40:09 +0530

    change opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
     siteName: "Personal Chatbot",
     images: [
       {
-        url: "/chatbot.png",
+        url: "/chatboot.png",
         width: 1200,
         height: 630,
         alt: "Personal Chatbot",
